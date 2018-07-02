d462e4fc79736365486f9fbe0639b6a3c59e4aec
Tim Savannah <kata198@gmail.com>
2018-07-01 20:22:00 -0400

    Add some relation methods to Meal and Person

--- tests/ichorORMTests/ichor_test_models/Meal.py
+++ tests/ichorORMTests/ichor_test_models/Meal.py
@@ -13,3 +13,33 @@ class Meal(DatabaseModel):
     # _CREATE_TABLE_SQL - Not a field to ichorORM, but used by the tests to create the tables
     _CREATE_TABLE_SQL = """CREATE TABLE meal(id serial PRIMARY KEY, food_group varchar(128) NULL, item_name varchar(255) NOT NULL, price decimal NOT NULL, id_person integer REFERENCES Person(id) NOT NULL, datasetUid varchar(255) NULL)"""
 
+    def getPerson(self):
+        '''
+            getPerson - get the Person object associated with this meal.
+
+                @return <ichor_test_models.Person/None> - 
+                    The Person object related to this meal, or None
+                     if none associated
+        '''
+
+        if not self.id_person:
+            return None
+
+        from ichor_test_models.Person import Person
+
+        return Person.get(self.id_person)
+
+    def getMeals(self):
+        '''
+            getMeals - get a list of all the Meal objects
+                associated with this person.
+
+                @return list<ichor_test_models.Meal> - A list of related Meal objects
+        '''
+        if not self.id:
+            # Not yet saved, no relations
+            return []
+
+        from ichor_test_models.Meal import Meal
+
+        return Meal.filter(id_person=self.id)
--- tests/ichorORMTests/ichor_test_models/Person.py
+++ tests/ichorORMTests/ichor_test_models/Person.py
@@ -13,3 +13,16 @@ class Person(DatabaseModel):
     # _CREATE_TABLE_SQL - Not a field to ichorORM, but used by the tests to create the tables
     _CREATE_TABLE_SQL = """CREATE TABLE person(id serial PRIMARY KEY, first_name varchar(255) NULL, last_name varchar(255) NOT NULL, eye_color varchar(64) NULL, age smallint NULL, birth_day smallint NULL, birth_month smallint NULL, datasetUid varchar(255) NULL)"""
 
+    def getFullName(self):
+        '''
+            getFullName - Returns the full name (first and last)
+
+              @return <str> - Full name
+        '''
+        firstName = self.first_name or ''
+        lastName = self.last_name or ''
+
+        # If only a first or last name, make sure we have no spaces
+        #   otherwise make it "%s %s" %(firstName, lastName)
+        ret = ' '.join([x for x in (firstName, lastName) if x])
+        return ret
